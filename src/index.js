a85479944a021ace063f32c69012cf0cbf5b46aa
Mary <32392607+MartFrida@users.noreply.github.com>
2023-11-14 23:12:07 +0100

    change select list

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import { btnFetch, catInfo, posts, loaderEl } from './refs.js';
+import { breedSelect, btnFetch, catInfo, posts, errorEl, loaderEl } from './refs.js';
 import { getBreeds, fetchCatByBreed } from './cat-api';
 import { renderMarkup, createCatCard } from './markup';
 
 // axios.defaults.headers.common["x-api-key"] = "live_gH6oeA8gIQy7k5AJMlE4mvn22i6yTQ8VIcgVQ9P7RurTz96AYtTfK0IWgF8YCmUg";
 
 export let items = [];
-// breedSelect.textContent='id'
-
+let idCat = breedSelect.value;
 
 btnFetch.addEventListener('click', onRenderCat)
 document.addEventListener("DOMContentLoaded", onRenderPage);
@@ -25,10 +24,19 @@ function onRenderPage() {
     });
 }
 
+//змінити респонс
 function onRenderCat() {
   loaderEl.classList.remove("is-hidden");
-  fetchCatByBreed('beng')
+  fetchCatByBreed(idCat)
     .then((res) => console.log(res))
     .catch(console.log)
     .finally(() => loaderEl.classList.add('is-hidden'))
 }
+
+// // get select ID
+// breedSelect.addEventListener('change', handleSelectId);
+
+// function handleSelectId() {
+//   idCat = breedSelect.value;
+//   return idCat;
+// }
--- src/markup.js
+++ src/markup.js
@@ -1,30 +1,24 @@
 import { items } from "./index.js";
-import { btnFetch, catInfo, posts, errorEl } from './refs.js';
+import { breedSelect, btnFetch, catInfo, posts, errorEl } from './refs.js';
 
 export function renderMarkup(data) {
-  const markup = data.map((cat) => createCatCard(cat)).join("");
-  catInfo.innerHTML = markup;
+  const markup = data.map((cat) => createCatsID(cat)).join("");
+  breedSelect.innerHTML = markup;
+  // як додати елемент в селект
 
   items.length === 0
     ? errorEl.classList.remove("is-hidden")
     : errorEl.classList.add("is-hidden");
 }
 
-export function createCatCard(cat) {
-  const { id, name, url } = cat;
+export function createCatsID(cat) {
+  const { id } = cat;
   return `
-  <div class="cat-card">
-  <img
-            src="${url}"
-            alt="${id}"
-          />
-          <div class="info">
-          <p class="cat-info">${id}</p>
-          <p class="cat-info">${name}</p>
-          </div>
-  </div>
+  <option value=${id}>${id}</option>
   `
-
 }
 
+export function createCatCard(id){
+  
+}
 
